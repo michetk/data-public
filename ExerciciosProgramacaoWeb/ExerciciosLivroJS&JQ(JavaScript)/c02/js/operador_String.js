1b7654a2a30eed710c7027de9c468b169a13a29c
Michel Miguel <michetkmiguel@gmail.com>
2019-02-02 01:23:07 -0200

    Add de exercicios, renomeação de pasta, e add README.md

---
+++ ExerciciosProgramacaoWeb/ExerciciosLivroJS&JQ(JavaScript)/c02/js/operador_String.js
@@ -0,0 +1,11 @@
+/*
+Usando Operadores de String
+*/
+
+var greeting = 'Howdy ';
+var name = 'Molly';
+
+var welcomeMessage = greeting + name + '!';
+
+var el = document.getElementById('greeting');
+el.textContent = welcomeMessage;
