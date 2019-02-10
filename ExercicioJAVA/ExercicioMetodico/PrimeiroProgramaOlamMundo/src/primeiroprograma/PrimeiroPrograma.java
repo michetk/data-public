d3a67aa21a49ec8a544e36009f88a586c428307e
Michel Miguel <michetkmiguel@gmail.com>
2019-02-10 12:49:52 -0200

    add exercicio JAVA

---
+++ ExercicioJAVA/ExercicioMetodico/PrimeiroProgramaOlamMundo/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+/*
+* Primeiro programa 'Olá Mundo'
+*/
+package primeiroprograma;
+
+/**
+ *
+ * @author Michel M
+ */
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+    }
+    
+}
