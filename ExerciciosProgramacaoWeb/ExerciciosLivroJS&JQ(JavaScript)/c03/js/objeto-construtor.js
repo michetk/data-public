7d7d111d7a60e782414bf7fd6caaa91a4bf14fe3
Michel Miguel <michetkmiguel@gmail.com>
2019-02-26 02:44:30 -0300

    add exercises JAVA-SCRIPT

---
+++ ExerciciosProgramacaoWeb/ExerciciosLivroJS&JQ(JavaScript)/c03/js/objeto-construtor.js
@@ -0,0 +1,16 @@
+/*
+*CRIANDO OBJETOS COM A SINTAXE DE CONSTRUTOR
+*/
+
+var hotel = new Object();
+hotel.name = 'Park';
+hotel.rooms = 120;
+hotel.booked = 77;
+hotel.checkAvailability = function() {
+  return this.rooms - this.booked;
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+var elRooms = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
