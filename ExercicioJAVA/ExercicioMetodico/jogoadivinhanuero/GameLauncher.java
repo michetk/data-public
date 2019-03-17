bad446f53850c4f33e835ecaa23a91333aeb3678
Michel Miguel <michetkmiguel@gmail.com>
2019-03-17 12:38:06 -0300

    jogo reproduzido do livro 'pense java' de Kathy Sierra e Bert Bates

---
+++ ExercicioJAVA/ExercicioMetodico/jogoadivinhanuero/GameLauncher.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogoadivinhanuero;
+
+/**
+ *
+ * @author Michel M
+ */
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
