72ba129f4bf6b8ad130a6c8156eef6387d231fea
Michel Miguel <Michel Miguel>
2020-03-22 09:03:27 -0300

    Files removeds

--- ExerciciosProgramacaoWeb/ExerciciosLivroHTML&CSS/chapter-03/README.md
+++
@@ -1,7 +0,0 @@
-# data-public
-
-Este repositório refere-se somente a publicações em exercícios de disciplinas acadêmica.
-
->Exercícios somente em HTML.
-
-m.m.d.s.s.
--- ExerciciosProgramacaoWeb/ExerciciosLivroJS&JQ(JavaScript)/c03/js/funcao-basica.js
+++
@@ -1,8 +0,0 @@
-// UMA FUNÇÃO BÁSICA
-
-var msg = 'Sign up to receive our newsoetter for 10% off!';
-function updateMessage() {
-  var el = document.getElementById('message')
-  el.textContent = msg;
-}
-updateMessage();
